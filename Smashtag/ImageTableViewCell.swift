1201080c4ebd94888df988649292a46cd46633ee
lalitote <beata.wasylkiewicz@gmail.com>
2016-12-02 11:46:51 +0100

    assignment 4 task 2
    adding custom cell to show image

---
+++ Smashtag/ImageTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageTableViewCell.swift
+//  Smashtag
+//
+//  Created by lalitote on 02.12.2016.
+//  Copyright © 2016 lalitote. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tweetImage: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+
+}
