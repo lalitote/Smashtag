ae4c78a1214260f1f44225d0dd8de374667e572e
lalitote <beata.wasylkiewicz@gmail.com>
2016-12-20 23:22:26 +0100

    assignment 4 extra task 6 (first steps)
    Adding to storyboard CollectionViewController, defining two new classes
    (one for controller and one for cell).

---
+++ Smashtag/ImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionViewCell.swift
+//  Smashtag
+//
+//  Created by lalitote on 20.12.2016.
+//  Copyright © 2016 lalitote. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+}
